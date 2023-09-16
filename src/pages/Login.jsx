47e9f4540603d1a5dd3b0ca36b8a23376544cff0
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-09-16 18:15:22 -0500

    logo autenticacion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import NavigationBar from '../components/navigationBar'; 
+import NavigationBar from '../components/navigationBar';
 
 const Login = () => {
 
@@ -26,36 +26,44 @@ const Login = () => {
 
 
   return (
-    <div><NavigationBar />
-      <div className="block mx-auto my-12 w-2/3 p-8 bg-white border border-gray-200 rounded-lg shadow-lg text-gray-800">
-        <h1 className="text-3xl font-bold text-center">Login</h1>
+    <div>
+      <NavigationBar />
+      <div class="flex justify-between items-center p-4">
+        <div className="w-2/4 flex justify-center ml-40">
+          <img src="./src/img/logo.png" alt="Logo" className="w-128 h-128" />
+        </div>
+        <div class="w-3/4">
+          <div className="block mx-auto my-12 w-2/3 p-8 bg-white border border-gray-200 rounded-lg shadow-lg text-gray-800">
+            <h1 className="text-3xl font-bold text-center">Login</h1>
 
-        <form className="mt-8" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="correo"
-            placeholder="Correo"
-            className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
-            required
-          />
+            <form className="mt-8" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="correo"
+                placeholder="Correo"
+                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                required
+              />
 
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
-            required
-          />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                required
+              />
 
-          <button
-            type="submit"
-            className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
-          >
-            Login
-          </button>
-          {error && <p className="text-red-500 font-bold text-lg my-3">* {error}</p>}
-        </form>
-        <p className='text-center'>¿No te has Registrado? <Link to="/register">Registrate</Link></p>
+              <button
+                type="submit"
+                className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
+              >
+                Login
+              </button>
+              {error && <p className="text-red-500 font-bold text-lg my-3">* {error}</p>}
+            </form>
+            <p className='text-center'>¿No te has Registrado? <Link to="/register">Registrate</Link></p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useNavigate, Link } from "react-router-dom";
-import NavigationBar from '../components/navigationBar'; 
+import NavigationBar from '../components/navigationBar';
 
 
 const Register = () => {
@@ -27,9 +27,9 @@ const Register = () => {
             setError("Por favor seleccione un archivo.");
             return; // Exit the function early
         }
-        
+
         const camposObligatorios = ['nombre', 'apellido', 'usuario', 'password', 'correo'];
-        
+
         for (const campo of camposObligatorios) {
             if (!eval(campo)) {
                 setError(`El campo '${campo.charAt(0).toUpperCase() + campo.slice(1)}' es obligatorio.`);
@@ -82,78 +82,85 @@ const Register = () => {
 
 
     return (
-    <div>
-        <NavigationBar />
+        <div>
+            <NavigationBar />
+            <div class="flex justify-between items-center p-4">
+                <div className="w-2/4 flex justify-center ml-40">
+                    <img src="./src/img/logo.png" alt="Logo" className="w-128 h-128" />
+                </div>
+                <div class="w-3/4">
 
-        <div className="block mx-auto my-12 w-2/3 p-8 bg-white border border-gray-200 rounded-lg shadow-lg text-gray-800">
+                    <div className="block mx-auto my-12 w-2/3 p-8 bg-white border border-gray-200 rounded-lg shadow-lg text-gray-800">
 
-            <h1 className="text-3xl font-bold text-center">Registrar Usuario</h1>
+                        <h1 className="text-3xl font-bold text-center">Registrar Usuario</h1>
 
-            <form onSubmit={handleSubmit}>
-                <input style={{ display: 'none' }} type="file" id="file" name="file" />
-                <label htmlFor="file" className="block text-center">
-                    <img src={Add} alt="addImage" className='mx-auto w-32 h-32 cursor-pointer' />
-                    <span className='text-gray-500 mt-2'>Subir Imagen</span>
-                </label>
+                        <form onSubmit={handleSubmit}>
+                            <input style={{ display: 'none' }} type="file" id="file" name="file" />
+                            <label htmlFor="file" className="block text-center">
+                                <img src={Add} alt="addImage" className='mx-auto w-32 h-32 cursor-pointer' />
+                                <span className='text-gray-500 mt-2'>Subir Imagen</span>
+                            </label>
 
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Nombre"
-                    className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Nombre"
+                                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
 
-                />
+                            />
 
-                <input
-                    type="text"
-                    name="apellido"
-                    placeholder="Apellido"
-                    className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                            <input
+                                type="text"
+                                name="apellido"
+                                placeholder="Apellido"
+                                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
 
-                />
+                            />
 
-                <input
-                    type="text"
-                    name="usuario"
-                    placeholder="Usuario"
-                    className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                            <input
+                                type="text"
+                                name="usuario"
+                                placeholder="Usuario"
+                                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
 
-                />
+                            />
 
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
 
-                />
+                            />
 
-                <input
-                    type="email"
-                    name="correo"
-                    placeholder="Correo"
-                    className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
+                            <input
+                                type="email"
+                                name="correo"
+                                placeholder="Correo"
+                                className="bg-gray-200 border border-gray-300 w-full text-lg placeholder-gray-600 p-2 my-2 focus:bg-white"
 
-                />
+                            />
 
-                <button
-                    type="submit"
-                    className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
-                >
-                    Registrarse
-                </button>
-                <button
-                    onClick={() => navigate("/RegisterProfesional")} // Use navigate to navigate
-                    className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
-                >
-                    Registrar Profesional
-                </button>
-                {error && <p className="text-red-500 font-bold text-lg my-3">* {error}</p>}
+                            <button
+                                type="submit"
+                                className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
+                            >
+                                Registrarse
+                            </button>
+                            <button
+                                onClick={() => navigate("/RegisterProfesional")} // Use navigate to navigate
+                                className="rounded-md bg-red-500 hover:bg-red-400 w-full text-lg text-white font-semibold py-2 my-3"
+                            >
+                                Registrar Profesional
+                            </button>
+                            {error && <p className="text-red-500 font-bold text-lg my-3">* {error}</p>}
 
 
-            </form>
-            <p className='text-center'>¿Ya te Registraste? <Link to={"/login"}>Login</Link></p>
-        </div>
+                        </form>
+                        <p className='text-center'>¿Ya te Registraste? <Link to={"/login"}>Login</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
